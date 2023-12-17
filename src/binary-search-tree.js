364e49e0a367a9aace6ffe3687d913ddcfd13d0d
z-e-a <e.a.zubkov@yandex.ru>
2023-12-17 17:56:02 +0300

    init: task 'basic-js-ds'

---
+++ src/binary-search-tree.js
@@ -0,0 +1,49 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+// const { Node } = require('../extensions/list-tree.js');
+
+/**
+* Implement simple binary search tree according to task description
+* using Node from extensions
+*/
+class BinarySearchTree {
+
+  root() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  add(/* data */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  has(/* data */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  find(/* data */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  remove(/* data */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  min() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  max() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+}
+
+module.exports = {
+  BinarySearchTree
+};
