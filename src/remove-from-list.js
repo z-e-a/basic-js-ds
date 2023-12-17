dcc62a23111a28e9bef8e87cffdc16cfe4099274
z-e-a <e.a.zubkov@yandex.ru>
2023-12-18 01:50:03 +0300

    feat: solve task 'remove-from-list'

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let resArr = [];
+  var curr = l;
+  while (curr.next !== null) {
+    if (curr.value != k) {
+      resArr.push(curr.value);
+    }
+    curr = curr.next;
+  }
+  if (curr.value != k) {
+    resArr.push(curr.value);
+  }
+  return convertArrayToList(resArr);
+}
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+    return new ListNode(cur);
+  }, null);
 }
 
 module.exports = {
