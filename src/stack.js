03c30768c8099a90d31af2c585c42e8d688b95b3
z-e-a <e.a.zubkov@yandex.ru>
2023-12-18 01:49:30 +0300

    feat: solve task 'stack'

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() {
+    this._array = [];
+  }
+
+  push(element) {
+    this._array.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._array.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._array[this._array.length - 1];
   }
 }
 
